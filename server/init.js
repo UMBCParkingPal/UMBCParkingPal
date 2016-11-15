4993d48a7924025648895516afe5d9752741712a
CFAK <constantinfakoehler@gmail.com>
2016-11-15 13:47:24 -0500

    Fixed Error in HomePage

--- server/init.js
+++ server/init.js
@@ -1,12 +1,4 @@
 import { Meteor } from 'meteor/meteor';
 Meteor.startup(function () {
-
+  
 });
-
-
-  // Meteor.loginWithGoogle({
-  //   requestPermissions: ['profile']
-  // }, function(err) {
-  //   if (err)
-  //     console.log(err);
-  // });
--- server/main.js
+++ server/main.js
@@ -1,11 +1,5 @@
 Accounts.config({ restrictCreationByEmailDomain: 'umbc.edu' })
 
-Accounts.onCreateUser(function(options,user){
-  console.log("create")
-  //   if (typeof(user.services.google) != "undefined") {
-  //       user.email = user.services.google.email;
-  //       user.profilePicture = user.services.google.picture;
-  //       user.username = user.services.google.name;
-  //   }
-  // return user;
+Accounts.onCreateUser((options, user) => {
+  console.log(user);
 });
