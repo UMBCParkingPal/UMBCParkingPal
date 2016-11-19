a421d0dfce83f89f4cb7e4951f56f3a2da382896
CFAK <constantinfakoehler@gmail.com>
2016-11-19 11:22:07 -0500

    Register backend

---
+++ client/Register.js
@@ -0,0 +1,25 @@
+Template.Register.events({
+  'submit form': function(){
+    event.preventDefault();
+
+    if(event.target.phone.value){
+      Meteor.users.update( {_id:Meteor.userId()},{$set: {'profile.phone' : event.target.phone.value}})
+    }
+
+    if(event.target.make.value){
+      Meteor.users.update( {_id:Meteor.userId()},{$set: {'profile.make' : event.target.make.value}})
+
+    }
+    if(event.target.model.value){
+      Meteor.users.update( {_id:Meteor.userId()},{$set: {'profile.model' : event.target.model.value}})
+
+    }
+    if(event.target.color.value){
+      Meteor.users.update( {_id:Meteor.userId()},{$set: {'profile.color' : event.target.color.value}})
+    }
+    if(event.target.plate.value){
+      Meteor.users.update( {_id:Meteor.userId()},{$set: {'profile.plate' : event.target.plate.value}})
+    }
+
+  }
+})
