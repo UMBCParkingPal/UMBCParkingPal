0a9bf8bfe5a707830d4cbe34e16bb0d85f5b9130
Braxton <bdubin1@umbc.edu>
2016-11-19 15:03:37 -0500

    homepage

--- client/Homepage.js
+++ client/Homepage.js
@@ -1,10 +1,24 @@
-Meteor.subscribe('parkingSpaces')
+Template.Home.onCreated(function(){
+	var self = this;
+	self.autorun(function() {
+		self.subscribe('parkingSpaces');
+	});
+});
 
-Template.ParkingSpaces.helpers({
+Template.Home.helpers({
 	parkingSpaces: ()=> {
     var thisId = Meteor.userId();
+		if (thisId) {
 		console.log("hello?",thisId);
+		};
 		return ParkingSpaces.find({});
 		// return ParkingSpaces.find({usersName == Meteor.user().profile.name});
+	}
+});
+
+
+Template.Home.events({
+	'click .new-parkingSpace': () => {
+		set('newParkingSpace', true);
 	}
 });
