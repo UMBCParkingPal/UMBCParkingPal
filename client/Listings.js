14aefe25524fbeffa58accca164f094d51f244c4
Abbie Minor <abminor1@umbc.edu>
2016-11-19 14:29:06 -0500

    Change how parking spots displayed

--- client/Listings.js
+++ client/Listings.js
@@ -1,14 +1,18 @@
-import '/imports/startup/accounts-config.js';
-
 Template.Listings.onCreated(function(){
 	var self = this;
-	self.autorun(function (){
+	self.autorun(function() {
 		self.subscribe('parkingSpaces');
 	});
 });
 
 Template.Listings.helpers({
-	shoppingList: ()=> {
-		return ParkingSpaces.find();
+	parkingSpaces: ()=> {
+		return ParkingSpaces.find({});
+	}
+});
+
+Template.Listings.events({
+	'click .new-parkingSpace': () => {
+		set('newParkingSpace', true);
 	}
 });
